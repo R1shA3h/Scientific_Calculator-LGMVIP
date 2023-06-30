f09426e273e6f9695b77075840f30154b591f317
Rishabh <99585764+R1shA3h@users.noreply.github.com>
2023-06-30 13:54:58 +0530

    Add files via upload

---
+++ ScientificCalculator/src/main/java/com/mycompany/scientificcalculator/ScientificCalculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.scientificcalculator;
+
+/**
+ *
+ * @author risha
+ */
+public class ScientificCalculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
